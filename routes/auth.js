b3ef9db0f60ff2f302962033369867ee4035fac0
Yogesh Yadav <2803yogesh.yadav@gmail.com>
2021-06-12 00:31:19 +0530

    Create auth route

---
+++ routes/auth.js
@@ -0,0 +1,30 @@
+const User = require('../models/user');
+var express = require('express');
+var router = express.Router();
+const { signup, signin, signout } = require('../controllers/auth');
+const { check } = require('express-validator');
+
+router.post(
+  '/signup',
+  check('name')
+    .isLength({ min: 5 })
+    .withMessage('must be at least 5 chars long'),
+  check('email').isEmail().withMessage('email is not correct'),
+  check('password')
+    .isLength({ min: 5 })
+    .withMessage('must be at least 5 chars long')
+    .matches(/\d/)
+    .withMessage('must contain a number'),
+  signup
+);
+
+router.post(
+  '/signin',
+  check('email').isEmail().withMessage('email is not correct'),
+  check('password').isLength({ min: 1 }).withMessage('Password is required'),
+  signin
+);
+
+router.get('/signout', signout);
+
+module.exports = router;
